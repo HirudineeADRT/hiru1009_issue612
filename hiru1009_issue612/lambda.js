01060707ad7bdb2c80efc3101c9f09cfe0b96ff9
HirudineeADRT <hirudinee@adroitlogic.com>
2019-09-10 13:37:27 +0530

    sample 11

--- hiru1009_issue612/lambda.js
+++ hiru1009_issue612/lambda.js
@@ -4,16 +4,7 @@ const redis = new SL_REDIS.Redis(clusterManager);
 
 exports.handler = function (event, context, callback) {
     // You must always quit the redis client after it's used
-    redis.type({
-        clusterIdentifier: 'test',
-        params: ['sample']
-    }, function (error, response, redisClient) {
-        if (error) {
-            callback(error);
-        } else {
-            //redisClient.quit();
-        }
-    });
+
 
     callback(null, { "message": "Successfully executed" });
 }
