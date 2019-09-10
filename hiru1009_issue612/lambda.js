6471566f06689966105ca54b29b1c820b6aab7e5
HirudineeADRT <hirudinee@adroitlogic.com>
2019-09-10 13:45:38 +0530

    remove unused

--- hiru1009_issue612/lambda.js
+++ hiru1009_issue612/lambda.js
@@ -1,7 +1,3 @@
-let SL_REDIS = require('slappforge-sdk-redis');
-let clusterManager = require('./ClusterManager');
-const redis = new SL_REDIS.Redis(clusterManager);
-
 exports.handler = function (event, context, callback) {
     // You must always quit the redis client after it's used
 
